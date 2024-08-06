5c5dd4fed56417e43f380207b604d5186da67f8a
Lesly Araya <lesarayamor@est.utn.ac.cr>
2024-08-06 16:52:59 -0600

    Se crea el paquete Comments
    Se crea la clase Comment

---
+++ Comments/Comment.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Comments;
+
+import Users.User;
+
+/**
+ *
+ * @author user
+ */
+public class Comment {
+    private int id;
+    private User user;
+    private String comment;
+
+    public int getId() {
+        return id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Comment(int id, User user, String comment) {
+        this.id = id;
+        this.user = user;
+        this.comment = comment;
+    }
+
+    public Comment() {
+        this(0,new User(),"");
+    }
+    
+    
+}
